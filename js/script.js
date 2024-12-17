6dbf80d98ec5237afe010862f7a02092885d27fd
domespa <dumiii1988@gmail.com>
2024-12-17 18:11:16 +0100

    fix: refactoring code js

--- js/script.js
+++ js/script.js
@@ -52,30 +52,6 @@ const nameElm = document.getElementById("name");
 const roleElm = document.getElementById("role");
 const emailElm = document.getElementById("email");
 const imageElm = document.getElementById("image");
-console.log(nameElm)
-console.log(roleElm)
-console.log(emailElm)
-
-
-// Stampa nomi
-let memberList = "";
-for(let i = 0; i < teamMembers.length; i++) {
-  const member = teamMembers[i];
-  memberList += `
-    <div id="card-container" class="col-4 flex-wrap">
-      <div id="card" class="text-white mb-3 d-flex">
-        <img id="contact-image" src="${member.img}" alt="${member.name}" class="img-fluid" width="35%">
-        <div class="card-body ps-3 pt-3">
-          <h5 id="full-name">${member.name}</h5>
-          <p id="role">${member.role}</p>
-          <p id="contact">${member.email}</p>
-        </div>
-      </div>
-    </div>
-    `;
-};
-
-cardsContainerElm.innerHTML = memberList;
 
 // FORM
 // DOM EVENTS
